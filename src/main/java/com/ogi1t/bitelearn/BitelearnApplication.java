fd52cf9f2d974e0371b611664bc6d1983ad61704
YuJeong <qordbwjd96@gmail.com>
2026-02-11 16:20:46 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/com/ogi1t/bitelearn/BitelearnApplication.java
@@ -0,0 +1,13 @@
+package com.ogi1t.bitelearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitelearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitelearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogi1t/bitelearn/BitelearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogi1t.bitelearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitelearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

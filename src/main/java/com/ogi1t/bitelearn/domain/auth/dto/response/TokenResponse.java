7d1b60dc72f2462ed425d5fe7ba8440e0fda3c3e
YuJeong <qordbwjd96@gmail.com>
2026-03-12 01:29:23 +0900

    feat: refreshToken Response에서 제외 #5

--- src/main/java/com/ogi1t/bitelearn/domain/auth/dto/response/TokenResponse.java
+++ src/main/java/com/ogi1t/bitelearn/domain/auth/dto/response/TokenResponse.java
@@ -1,5 +1,6 @@
 package com.ogi1t.bitelearn.domain.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +17,9 @@ public class TokenResponse {
 
   private String grantType;    // 보통 "Bearer"
   private String accessToken;  // JWT Access Token
+
+  @JsonIgnore // 응답 바디(JSON) 직렬화 시 이 필드를 제외시킨다!
   private String refreshToken; // JWT Refresh Token
+
   private Long accessTokenExpiresIn; // 액세스 토큰 만료 시간 (밀리초)
 }

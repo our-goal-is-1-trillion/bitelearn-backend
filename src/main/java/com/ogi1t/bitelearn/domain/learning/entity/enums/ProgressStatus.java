761dd8e89898c99cd94428557800e7e67a220817
YuJeong <qordbwjd96@gmail.com>
2026-03-18 11:31:34 +0900

    feat: enum ProgressStatus 구현 #11

---
+++ src/main/java/com/ogi1t/bitelearn/domain/learning/entity/enums/ProgressStatus.java
@@ -0,0 +1,7 @@
+package com.ogi1t.bitelearn.domain.learning.entity.enums;
+
+public enum ProgressStatus {
+  READY,              // 시작 전 (단어장 단계)
+  QUIZ_IN_PROGRESS,   // 퀴즈 푸는 중
+  COMPLETED           // 챕터 완료
+}

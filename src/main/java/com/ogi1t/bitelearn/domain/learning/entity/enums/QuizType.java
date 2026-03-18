172e30bd2cf32773b20a4e29bc5a043e896f3c30
YuJeong <qordbwjd96@gmail.com>
2026-03-18 11:32:02 +0900

    feat: enum QuizType 구현 #11

---
+++ src/main/java/com/ogi1t/bitelearn/domain/learning/entity/enums/QuizType.java
@@ -0,0 +1,9 @@
+package com.ogi1t.bitelearn.domain.learning.entity.enums;
+
+public enum QuizType {
+  TEXT_MCQ,       // 일반 객관식
+  DOC_SELECT,     // 문서 선택형 객관식
+  DOC_MULTI,      // 문서 다지선다형
+  DIALOGUE_MCQ,   // 대화형 객관식
+  DIALOGUE_OX     // 대화형 OX
+}

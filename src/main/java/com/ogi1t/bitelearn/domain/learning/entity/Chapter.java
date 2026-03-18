643ea06863dab71a6f90ce8c1adc69e91a7d5ce4
YuJeong <qordbwjd96@gmail.com>
2026-03-18 17:06:29 +0900

    feat: Entity 4가지 추가 수정사항 반영 #21
    - 프롤로그 소제목
    - 프롤로그 내용
    - 이번 목표
    - 핵심 내용 키워드

--- src/main/java/com/ogi1t/bitelearn/domain/learning/entity/Chapter.java
+++ src/main/java/com/ogi1t/bitelearn/domain/learning/entity/Chapter.java
@@ -3,6 +3,7 @@ package com.ogi1t.bitelearn.domain.learning.entity;
 import com.ogi1t.bitelearn.domain.learning.entity.enums.Category;
 import com.ogi1t.bitelearn.domain.learning.entity.enums.Topic;
 import jakarta.persistence.*;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +22,15 @@ public class Chapter {
   private Topic topic;
 
   private String title;
+
+  private String prologueSubtitle; // 프롤로그 소제목
+
+  @Column(columnDefinition = "TEXT")
+  private String prologueContent; // 프롤로그 내용
+
+  private String currentGoal; // 이번 목표
+
+  private String coreKeywords; // 핵심 내용 키워드
 
   private Integer sequence; // 챕터 순서 (1, 2, 3...)
 }

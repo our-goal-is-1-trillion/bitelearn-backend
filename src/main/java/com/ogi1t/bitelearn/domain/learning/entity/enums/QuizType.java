b59f7bafad4023ff8577fda2f4299c247f2f6091
YuJeong <qordbwjd96@gmail.com>
2026-03-19 14:39:17 +0900

    feat: QuizType enum 명 변경 #25

--- src/main/java/com/ogi1t/bitelearn/domain/learning/entity/enums/QuizType.java
+++ src/main/java/com/ogi1t/bitelearn/domain/learning/entity/enums/QuizType.java
@@ -2,8 +2,8 @@ package com.ogi1t.bitelearn.domain.learning.entity.enums;
 
 public enum QuizType {
   TEXT_MCQ,       // 일반 객관식
-  DOC_SELECT,     // 문서 선택형 객관식
-  DOC_MULTI,      // 문서 다지선다형
+  DOC_CLICK,      // 문서 클릭형 객관식
+  DOC_MCQ,        // 문서 객관식
   DIALOGUE_MCQ,   // 대화형 객관식
   DIALOGUE_OX     // 대화형 OX
 }
